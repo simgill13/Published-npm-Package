251d165d5515a9933ee8fbeb2e857c78d87311d0
Sim Gill <simgill89@gmail.com>
2018-10-19 15:11:00 -0700

    n

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Second from './second';
 // import Alert from './alert';
 import InputBanner from './InputBanner';
+import './sim.css'
 
 
 class Sim extends React.Component {
@@ -14,7 +15,7 @@ class Sim extends React.Component {
       }
   render() {
     return (
-      <div style={{backgroundColor:"red"}} >This is  {this.state.name}! THIS IS A COMPONENT FROM NPM MODULE</div>
+      <div className='simtest' >This is  {this.state.name}! THIS IS A COMPONENT FROM NPM MODULE</div>
     );
   }
 }
